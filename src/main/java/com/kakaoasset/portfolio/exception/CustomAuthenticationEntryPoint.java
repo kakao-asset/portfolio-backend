1fa920336467d4011ac06efcbd14b5dd41bfd21c
yuri.ji <dbfl9711@gmail.com>
2022-11-11 16:31:16 +0900

    카카오 로그인 기능 추가 (#1)

---
+++ src/main/java/com/kakaoasset/portfolio/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,43 @@
+package com.kakaoasset.portfolio.exception;
+
+import com.kakaoasset.portfolio.jwt.JwtProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        String exception = (String) request.getAttribute("exception");
+        String errorCode;
+
+        if(exception.equals("null")) {
+            errorCode = "헤더에 토큰이 존재하지 않습니다.";
+            setResponse(response, errorCode);
+        }
+
+        else if(exception.equals("토큰이 만료되었습니다.")) {
+            errorCode = "토큰이 만료되었습니다.";
+            setResponse (response, errorCode);
+        }
+
+        else if(exception.equals("유효하지 않은 토큰입니다.")) {
+            errorCode = "유효하지 않은 토큰입니다.";
+            setResponse(response, errorCode);
+        }
+    }
+
+    private void setResponse(HttpServletResponse response, String errorCode) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().println(errorCode);
+    }
+}
+
