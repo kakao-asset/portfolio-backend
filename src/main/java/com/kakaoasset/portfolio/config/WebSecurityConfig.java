b7defe4962f3efcbef9dd25e6609d76d4095b3a6
yuri.ji <dbfl9711@gmail.com>
2022-11-16 19:11:21 +0900

    cors 적용

--- src/main/java/com/kakaoasset/portfolio/config/WebSecurityConfig.java
+++ src/main/java/com/kakaoasset/portfolio/config/WebSecurityConfig.java
@@ -61,10 +61,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:3000/");
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
